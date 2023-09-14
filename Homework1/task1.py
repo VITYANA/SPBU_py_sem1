5fc383d04aef0e731b8cb3536d30ab9894c8f016
Khanukaev Viktor Vasilyevich <144259984+VITYANA@users.noreply.github.com>
2023-09-14 22:02:06 +0300

    add homework1

---
+++ Homework1/task1.py
@@ -0,0 +1,8 @@
+def multiply(x_):
+    x_square = x_ ** 2
+    return (x_square + x_) * (x_square + 1) + 1
+
+
+if __name__ == "__main__":
+    x = int(input("Введите x: "))
+    print(f"{x} ^ 4 + {x} ^ 3 + {x} ^ 2 + {x} + 1 =", multiply(x))
