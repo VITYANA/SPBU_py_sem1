7da145f75073ba1cd1ccb95f93a438473951dfdf
Khanukaev Viktor Vasilyevich <144259984+VITYANA@users.noreply.github.com>
2023-09-16 22:47:33 +0300

    fixed bash

--- Homework1/bash.py
+++ Homework1/bash.py
@@ -8,46 +8,43 @@ def find_file(name, directory):
             return os.path.join(root, name)
 
 
-def return_head_lines(string, parameter):
+def return_head_lines(file, parameter):
+    final_list = []
     i = 0
-    current_strings = 0
-    final_string = ''
-    while i != len(string) and current_strings != parameter:
-        if string[i] == '\n':
-            final_string += string[i]
-            current_strings += 1
+    with open(file) as file_in:
+        for lines in file_in:
+            if i == parameter:
+                break
+            final_list.append(lines)
             i += 1
-        else:
-            final_string += string[i]
-            i += 1
-    return final_string
+    return "".join(final_list)
 
 
-def return_head_bytes(string, parameter):
-    final_string = ''
-    current_bytes = 0
+def return_head_bytes(file, parameter):
+    final_arr = b''
     i = 0
-    while i != len(string) and current_bytes != parameter:
-        final_string += string[i]
-        i += 1
-        current_bytes += len(string[i].encode('utf-8'))
-    return final_string
+    with open(file) as file_in:
+        for lines in file_in:
+            while i != parameter:
+                final_arr += lines[i].encode()
+                i += len(lines[i])
+    return final_arr.decode()
 
 
-def return_tail_lines(string, parameter):
-    final_string = ''
-    reversed_string = string[::-1]
+def return_tail_lines(file, parameter):
+    final_list = []
+    reversed_list = open(file).read()[::-1]
     i = 0
     current_lines = 0
-    while i != len(string) and current_lines != parameter:
-        if reversed_string[i] == "\n":
-            final_string += reversed_string[i]
+    while i != len(file) and current_lines != parameter:
+        if reversed_list[i] == "\n":
+            final_list.append(reversed_list[i])
             i += 1
             current_lines += 1
         else:
-            final_string += reversed_string[i]
+            final_list.append(reversed_list[i])
             i += 1
-    return final_string[::-1]
+    return "".join(final_list[::-1])
 
 
 def return_tail_bytes(string, parameter):
@@ -66,27 +63,36 @@ if __name__ == "__main__":
     file_get = find_file(sys.argv[-1], "/")
     main_function_name = (sys.argv[1])
     args = sys.argv[2:-1]
-    file = open(file_get).read()
     if main_function_name == "wc":
         result = []
+        l = 1
+        w = 0
+        c = 0
+        m = 0
+        with open(file_get) as file_start:
+            for lines in file_start:
+                if "\n" in lines:
+                    l += 1
+                w += lines.count(" ")
+                m += len(lines)
         if "-l" in args:
-            result.append(file.count("\n"))
+            result.append(l)
         if "-w" in args:
-            result.append(file.count(" "))
+            result.append(w + l)
         if "-c" in args:
             result.append(os.path.getsize(file_get))
         if "-m" in args:
-            result.append(len(file))
+            result.append(m)
         print(*result, sys.argv[-1])
     elif main_function_name == "head":
         number = int(sys.argv[3])
         if "-n" in args:
-            print(return_head_lines(file, number))
+            print(return_head_lines(file_get, number))
         elif "-c" in args:
-            print(return_head_bytes(file, number))
+            print(return_head_bytes(file_get, number))
     elif main_function_name == "tail":
         number = int(sys.argv[3])
         if "-n" in args:
-            print(return_tail_lines(file, number))
+            print(return_tail_lines(file_get, number))
         elif "-c" in args:
-            print(return_tail_bytes(file, number))
+            print(return_tail_bytes(file_get, number))
