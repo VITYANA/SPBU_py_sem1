b4d7c975f015368d7bff8d662a77546f0685fa82
Khanukaev Viktor Vasilyevich <144259984+VITYANA@users.noreply.github.com>
2023-09-20 21:59:32 +0300

    Upgrade hw2_task2

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
 import csv
 
+
 def find_matches(file_in):
-    words_count = []
+    words_count = {}
     with open(file_in) as file:
         for lines in file:
-            lines = sorted(lines.split())
-            i = 0
-            while i != len(lines):
-                if lines[i] not in words_count:
-                    words_count.append([lines[i], int(lines.count(lines[i]))])
-                    i += lines.count(lines[i])
-    i = 0
-    while i != len(words_count):
-        j = i + 1
-        while j != len(words_count):
-            if words_count[j][0] == words_count[i][0]:
-                words_count[i][1] += words_count[j][1]
-                del words_count[j]
-            else:
-                j += 1
-        words_count[i][1] = str(words_count[i][1])
-        i += 1
+            for element in lines.split():
+                if words_count.get(element, None):
+                    words_count[element] += 1
+                else:
+                    words_count[element] = 1
     return words_count
 
 
 if __name__ == "__main__":
     file_path_in = input("Введите путь до файла, подлежащего анализу: ")
     file_path_out = input("Введите путь до файла, в который нужно передать результаты анализа: ")
-    final_str = find_matches(file_path_in)
+    final_dict = find_matches(file_path_in)
     with open(file_path_out, "a") as csv_file:
         writer = csv.writer(csv_file)
-        for i in range(len(final_str)):
-            writer.writerow(final_str[i])
+        writer.writerows(final_dict.items())
