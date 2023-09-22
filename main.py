320d6ff4dfa9c6fd579b547476268a85f9bab6d4
Khanukaev Viktor Vasilyevich <144259984+VITYANA@users.noreply.github.com>
2023-09-22 14:41:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+
+
+if __name__ == "__main__":
+    print("Добро пожаловать в мою игру, в ней я загадаю вам число формата 0000, а вы должны его отгадать. \n"
+          "Правила:\n"
+          "1)В случае, когда вы угадаете цифру, но она будет на другой позиции, я выведу A - 1.\n"
+          "2)В случае, когда вы угадаете цифру и её позицию, я выведу B - 1.\n"
+          "Все цифры в числе разные.\n"
+          "Удачи! ")
+    hidden_num = ''
+    while len(hidden_num) != 4:
+        number = str(random.randint(0, 9))
+        if number not in hidden_num:
+            hidden_num += number
+    print('\n--НАЧАЛО ИГРЫ--\n'
+          '\nВводите числа!')
+    guessed_num = ''
+    turn_num = 0
+    while guessed_num != hidden_num:
+        turn_num += 1
+        a = 0
+        b = 0
+        guessed_num = input()
+        if len(guessed_num) != 4:
+            while len(guessed_num) != 4:
+                guessed_num = input("Неверный формат числа!\nВведите число типа 0000\n")
+        for i in range(4):
+            if guessed_num[i] in hidden_num:
+                if guessed_num[i] == hidden_num[i]:
+                    b += 1
+                else:
+                    a += 1
+        print(f'{turn_num}: A - {a} | B - {b}')
+    print(f"Верно, поздавляю!\nКоличество попыток - {turn_num}")
