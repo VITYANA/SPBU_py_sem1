c4dc2683694934be895906d41b53885a94b3ff3d
Khanukaev Viktor Vasilyevich <144259984+VITYANA@users.noreply.github.com>
2023-09-25 19:09:27 +0300

    Add files via upload

--- src/Homeworks/homework2/task2.py
+++ src/Homeworks/homework2/task2.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 
 def find_matches(file_in):
@@ -6,7 +7,7 @@ def find_matches(file_in):
     with open(file_in) as file:
         for lines in file:
             for element in lines.split():
-                if words_count.get(element, None):
+                if element in words_count:
                     words_count[element] += 1
                 else:
                     words_count[element] = 1
@@ -15,8 +16,13 @@ def find_matches(file_in):
 
 if __name__ == "__main__":
     file_path_in = input("Введите путь до файла, подлежащего анализу: ")
+    while not os.path.exists(file_path_in):
+        file_path_in = input("Путь до файла, подлежащего анализу, некорректный, попробуйте ещё раз:\n")
     file_path_out = input("Введите путь до файла, в который нужно передать результаты анализа: ")
+    while not os.path.exists(file_path_out):
+        file_path_out = input("Введите путь до файла, в который нужно передать результаты анализа, некорректный, попробуйте ещё раз:\n")
     final_dict = find_matches(file_path_in)
-    with open(file_path_out, "a") as csv_file:
+    with open(file_path_out, "w") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerows(final_dict.items())
+    print("Результаты анализа записаны.")
