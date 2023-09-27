c4778a5a4853f8d2f42fcf402ad522aa06440328
Виктор Ханукаев <144259984+VITYANA@users.noreply.github.com>
2023-09-27 22:28:39 +0300

    modified homework2 task2

--- src/Homeworks/homework2/task2/task2.py
+++ src/Homeworks/homework2/task2/task2.py
@@ -17,10 +17,16 @@ def find_matches(file_in):
 if __name__ == "__main__":
     file_path_in = input("Введите путь до файла, подлежащего анализу: ")
     while not os.path.exists(file_path_in):
-        file_path_in = input("Путь до файла, подлежащего анализу, некорректный, попробуйте ещё раз:\n")
-    file_path_out = input("Введите путь до файла, в который нужно передать результаты анализа: ")
+        file_path_in = input(
+            "Путь до файла, подлежащего анализу, некорректный, попробуйте ещё раз:\n"
+        )
+    file_path_out = input(
+        "Введите путь до файла, в который нужно передать результаты анализа: "
+    )
     while not os.path.exists(file_path_out):
-        file_path_out = input("Введите путь до файла, в который нужно передать результаты анализа, некорректный, попробуйте ещё раз:\n")
+        file_path_out = input(
+            "Введите путь до файла, в который нужно передать результаты анализа, некорректный, попробуйте ещё раз:\n"
+        )
     final_dict = find_matches(file_path_in)
     with open(file_path_out, "w") as csv_file:
         writer = csv.writer(csv_file)
