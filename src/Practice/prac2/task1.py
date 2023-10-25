7a8fb13c4fc64fea24d022c6ce33b17a3e5bdc6b
Виктор Ханукаев <144259984+VITYANA@users.noreply.github.com>
2023-10-25 21:11:41 +0300

    Separate on functions, fix print, add check of input, modified algorithm

--- src/Practice/prac2/task1.py
+++ src/Practice/prac2/task1.py
@@ -1,16 +1,36 @@
-if __name__ == "__main__":
+def main():
     n = int(input("Введите число - предел для знаменателя: "))
+    check_input(n)
+    all_nums = find_answers(n)
+    result = sort_nums(all_nums)
+    print("\n".join(map(str, result)))
+
+
+def sort_nums(lst):
+    return sorted(lst, key=lambda x: float(x[x.find("=") + 2 :]))
+
+
+def find_answers(n):
     all_nums = []
     for i in range(1, n):
         check = True
-        local_nums = []
         for j in range(i + 1, n + 1):
             for divider in range(2, n):
                 if i % divider == 0 and j % divider == 0:
                     check = False
-            if check == True and str(i) + "/" + str(j) not in all_nums:
+            if check and i / j not in all_nums:
                 res = i / j
-                all_nums.append([i, j, res])
-    all_nums = sorted(all_nums, key=lambda x: x[2])
-    for i in range(len(all_nums)):
-        print(str(all_nums[i][0]) + "/" + str(all_nums[i][1]))
+                all_nums.append(f"{i} / {j} = {res}")
+    return all_nums
+
+
+def check_input(n):
+    try:
+        int(n)
+        return True
+    except TypeError:
+        raise TypeError(f"{n} isn't integer.")
+
+
+if __name__ == "__main__":
+    main()
