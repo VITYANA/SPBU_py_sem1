2f6662ca7b4bafaa951a0606e4911efcc2376aba
Виктор Ханукаев <144259984+VITYANA@users.noreply.github.com>
2023-10-26 20:57:14 +0300

    Delete requirements, fix Errors type, fix functions return type, add more tests, fix tests

--- src/Practice/prac6/task1.py
+++ src/Practice/prac6/task1.py
@@ -5,10 +5,10 @@ def main_func():
     except Exception as err:
         print(f"Incorrect input: {err}")
         return 0
-    a, b, c = float(args[0]), float(args[1]), float(args[2])
+    a, b, c = map(float, args)
     try:
         result = solve_equation(a, b, c)
-        print(f"Solution of the equation: {''.join(map(str, result))}")
+        print(f"Solution of the equation: {', '.join(map(str, result))}")
     except Exception as err:
         print(f"Program ended with error: {err}")
         return 0
@@ -16,7 +16,7 @@ def main_func():
 
 def check_input(args):
     if len(args) != 3:
-        raise AttributeError("You need to input 3 args!")
+        raise ValueError("You need to input 3 args!")
     for i in args:
         if not is_number(i):
             raise ValueError(f"{i} isn't number!")
@@ -25,9 +25,9 @@ def check_input(args):
 
 def solve_equation(a, b, c):
     if a == b == c == 0:
-        return "Solve is any real number."
+        raise ValueError("All coefficient cant be zero.")
     elif a == b == 0:
-        raise ValueError("Wrong parameters. a and b cant be zero.")
+        raise ValueError("Wrong parameters. a and b cant be zero simultaneously.")
     elif a == 0:
         return linear_equation_solve(b, c)
     else:
@@ -36,21 +36,19 @@ def solve_equation(a, b, c):
 
 def quadratic_equation_solve(a, b, c):
     discriminant = b**2 - 4 * a * c
-    if discriminant > 0:
-        res = (
-            f"x1 = {((-b + discriminant ** 0.5) / (2 * a))} ",
-            f"x2 = {((-b - discriminant ** 0.5) / (2 * a))}",
-        )
-        return res
-    elif discriminant == 0:
-        return tuple(f"x = {-b / (2 * a)}")
-    else:
-        raise ValueError("Negative discriminant, no solves.")
+    if discriminant == 0:
+        return ((-b / (2 * a)),)
+    elif discriminant < 0:
+        raise ArithmeticError("Negative discriminant, no solves.")
+    res = (
+        ((-b + discriminant**0.5) / (2 * a)),
+        ((-b - discriminant**0.5) / (2 * a)),
+    )
+    return res
 
 
 def linear_equation_solve(k, b):
-    res = (f"x = {-b / k}",)
-    return res
+    return ((-b / k),)
 
 
 def is_number(arg):
