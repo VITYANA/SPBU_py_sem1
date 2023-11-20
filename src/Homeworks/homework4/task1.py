35edca9abbfb7b803e3c994a46fb06dd6593e83e
Виктор Ханукаев <144259984+VITYANA@users.noreply.github.com>
2023-11-20 18:31:59 +0300

    fix names of functions and variables, '\n' in print, modified functions cycles, add early return, exception.

--- src/Homeworks/homework4/task1.py
+++ src/Homeworks/homework4/task1.py
@@ -1,88 +1,84 @@
-def binary_transform(num, bit_count):
-    origin_num = num
-    num = abs(num)
+def transform_from_decimal_to_binary(num, bit_count):
+    original_num = num
+    original_num = abs(original_num)
     binary_num = ""
-    while num:
-        binary_num += str(num % 2)
-        num //= 2
+    while original_num > 0:
+        binary_num += str(original_num % 2)
+        original_num //= 2
         if len(binary_num) > bit_count - 1:
             raise ValueError("Overflow")
+    binary_num = add_zero(binary_num, bit_count)
+    if num >= 0:
+        binary_num_sign = "0"
+    else:
+        binary_num_sign = "1"
+    binary_num += binary_num_sign
+    return binary_num[::-1]
+
+
+def add_zero(binary_num, bit_count):
     while len(binary_num) < bit_count - 1:
         binary_num += "0"
-    if origin_num >= 0:
-        binary_num += "0"
-    else:
-        binary_num += "1"
-    return binary_num[::-1]
+    return binary_num
 
 
 def reverse_binary_transform(binary_num):
     reverse_binary_num = binary_num
-    if binary_num[0] == "1":
-        binary_num = binary_num[::-1]
-        reverse_binary_num = ""
-        for i in range(len(binary_num) - 1):
-            if binary_num[i] == "0":
-                reverse_binary_num += "1"
-            else:
-                reverse_binary_num += "0"
-        reverse_binary_num += "1"
-        reverse_binary_num = reverse_binary_num[::-1]
+    if not (binary_num[0] == "1"):
+        return reverse_binary_num
+    binary_num = binary_num[::-1]
+    reverse_binary_num = ""
+    for i in range(len(binary_num) - 1):
+        if binary_num[i] == "0":
+            reverse_binary_num += "1"
+        else:
+            reverse_binary_num += "0"
+    reverse_binary_num += "1"
+    reverse_binary_num = reverse_binary_num[::-1]
     return reverse_binary_num
 
 
 def additional_binary_transform(reverse_bin_num):
     additional_binary_num = reverse_bin_num
-    if reverse_bin_num[0] == "1":
-        reverse_bin_num = reverse_bin_num[::-1]
-        add_1 = True
-        additional_binary_num = ""
-        for i in range(len(reverse_bin_num)):
-            if reverse_bin_num[i] == "1":
-                if add_1:
-                    additional_binary_num += "0"
-                else:
-                    additional_binary_num += "1"
-            else:
-                if add_1:
-                    additional_binary_num += "1"
-                    add_1 = False
-                else:
-                    additional_binary_num += "0"
-        additional_binary_num = additional_binary_num[::-1]
-    return additional_binary_num
+    if not reverse_bin_num[0] == "1":
+        return additional_binary_num
+    reverse_bin_num = reverse_bin_num[::-1]
+    additional_binary_num = ""
+    carry = 1
+    for i in range(len(reverse_bin_num)):
+        additional_binary_num += str((int(reverse_bin_num[i]) + carry) % 2)
+        carry = (int(reverse_bin_num[i]) + carry) // 2
+    return additional_binary_num[::-1]
 
 
 def sum_in_binary(num_1, num_2):
     num_1 = num_1[::-1]
     num_2 = num_2[::-1]
     sum_result = ""
-    need_to_plus_1 = False
+    carry = 0
     for i in range(len(num_1)):
-        if need_to_plus_1:
-            sum_result += str((int(num_1[i]) + int(num_2[i]) + 1) % 2)
-            if (int(num_1[i]) + int(num_2[i]) + 1) <= 1:
-                need_to_plus_1 = False
-        else:
-            sum_result += str((int(num_1[i]) + int(num_2[i])) % 2)
-            if int(num_1[i]) + int(num_2[i]) > 1:
-                need_to_plus_1 = True
-    sum_result = sum_result[::-1]
-    return sum_result
+        sum_result += str((int(num_1[i]) + int(num_2[i]) + carry) % 2)
+        carry = (int(num_1[i]) + int(num_2[i]) + carry) // 2
+    return sum_result[::-1]
+
+
+def get_binary_from_additional(binary, depth):
+    result_str = binary
+    if not binary[0] == "1":
+        return result_str[::-1]
+    result_str = reverse_binary_transform(binary)
+    result_str = sum_in_binary(
+        result_str,
+        additional_binary_transform(
+            reverse_binary_transform(transform_from_decimal_to_binary(1, depth))
+        ),
+    )
+    return result_str[::-1]
 
 
 def binary_to_decimal(binary, depth):
-    result_str = binary
-    if binary[0] == "1":
-        result_str = reverse_binary_transform(binary)
-        result_str = sum_in_binary(
-            result_str,
-            additional_binary_transform(
-                reverse_binary_transform(binary_transform(1, depth))
-            ),
-        )
+    result_str = get_binary_from_additional(binary, depth)
     result_num = 0
-    result_str = result_str[::-1]
     for i in range(len(result_str) - 1):
         result_num += int(result_str[i]) * 2**i
     if binary[0] == "1":
@@ -90,9 +86,26 @@ def binary_to_decimal(binary, depth):
     return result_num
 
 
+def check_input(num1, num2, bit_count):
+    check_flag = True
+    try:
+        transform_from_decimal_to_binary(num1, bit_count)
+    except Exception as err:
+        check_flag = False
+        print(f"{err}: Can't transform {num1} to binary with {bit_count} bit count.")
+    try:
+        transform_from_decimal_to_binary(num2, bit_count)
+    except Exception as err:
+        print(f"{err}: Can't transform {num2} to binary with {bit_count} bit count.")
+        check_flag = False
+    return check_flag
+
+
 def results_out(num1, num2, bit_count):
-    binary_number1 = binary_transform(num1, bit_count)
-    binary_number2 = binary_transform(num2, bit_count)
+    if not check_input(num1, num2, bit_count):
+        return 0
+    binary_number1 = transform_from_decimal_to_binary(num1, bit_count)
+    binary_number2 = transform_from_decimal_to_binary(num2, bit_count)
     reverse_binary_number1 = reverse_binary_transform(binary_number1)
     reverse_binary_number2 = reverse_binary_transform(binary_number2)
     additional_binary_num1 = additional_binary_transform(reverse_binary_number1)
@@ -100,23 +113,29 @@ def results_out(num1, num2, bit_count):
     sum_of_nums = sum_in_binary(additional_binary_num1, additional_binary_num2)
     decimal_num_sum = binary_to_decimal(sum_of_nums, bit_count)
     num2_for_differ = additional_binary_transform(
-        reverse_binary_transform(binary_transform(-num2, bit_count))
+        reverse_binary_transform(transform_from_decimal_to_binary(-num2, bit_count))
     )
     differ = sum_in_binary(additional_binary_num1, num2_for_differ)
     decimal_num_differ = binary_to_decimal(differ, bit_count)
+    print(f"numbers in binary:")
+    print(f"   1) binary x: {binary_number1}")
+    print(f"      binary y: {binary_number2}")
+    print(f"   2) reverse binary x: {reverse_binary_number1}")
+    print(f"      reverse binary y: {reverse_binary_number2}")
+    print(f"   3) additional binary x: {additional_binary_num1}")
+    print(f"      additional binary y: {additional_binary_num2}")
+    print(f"sum:")
     print(
-        f"numbers in binary:\n   1) binary x: {binary_number1}\n      binary y: {binary_number2}\n"
-        f"   2) reverse binary x: {reverse_binary_number1}\n      reverse binary y: {reverse_binary_number2}\n"
-        f"   3) additional binary x: {additional_binary_num1}\n      additional binary y: {additional_binary_num2}\n"
-        f"sum:\n   in binary: {additional_binary_num1} + {additional_binary_num2} = {sum_of_nums}\n"
-        f"   in decimal: {num1} + {num2} = {decimal_num_sum}\n"
-        f"differ:\n   in binary: {additional_binary_num1} - {num2_for_differ} = {differ}\n"
-        f"   in decimal: {num1} - {num2} = {decimal_num_differ}"
+        f"   in binary: {additional_binary_num1} + {additional_binary_num2} = {sum_of_nums}"
     )
+    print(f"   in decimal: {num1} + {num2} = {decimal_num_sum}")
+    print(f"differ:")
+    print(f"   in binary: {additional_binary_num1} - {num2_for_differ} = {differ}")
+    print(f"   in decimal: {num1} - {num2} = {decimal_num_differ}")
 
 
 if __name__ == "__main__":
-    number_1 = int(input("Введите 1 число: "))
-    number_2 = int(input("Введите 2 число: "))
-    bit_depth = int(input("Введите количество бит в представлении числа: "))
+    number_1 = int(input("Enter first number: "))
+    number_2 = int(input("Enter second number: "))
+    bit_depth = int(input("Enter bit count in your numbers in binary: "))
     results_out(number_1, number_2, bit_depth)
