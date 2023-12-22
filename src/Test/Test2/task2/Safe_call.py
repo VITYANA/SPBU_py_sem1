20be3f94bdc385d096d33f053fa934acbe0e16a6
Виктор Ханукаев <144259984+VITYANA@users.noreply.github.com>
2023-12-22 22:31:49 +0300

    Modifie program and add more tests.

--- src/Test/Test2/task2/Safe_call.py
+++ src/Test/Test2/task2/Safe_call.py
@@ -12,9 +12,12 @@ def safe_call(function):
             return result
         except Exception:
             func_name = function.__name__
-            error_type, message_of_error = str(sys.exc_info()[0]), sys.exc_info()[1]
-            error_type = error_type[error_type.find("'") : -1]
-            error_line, error_line_num = extract_error_line_from_stacktrace()
+            (
+                error_line,
+                error_line_num,
+                error_type,
+                message_of_error,
+            ) = extract_error_line_from_stacktrace()
             output_message = return_message(
                 func_name, error_type, message_of_error, error_line, error_line_num
             )
@@ -23,11 +26,12 @@ def safe_call(function):
     return inner
 
 
-def return_message(func_name, type_error, message_error, error_line, error_line_num):
+def return_message(func_name, error_type, message_of_error, error_line, error_line_num):
     return_value = (
-        f"\nerror in function: {func_name}\n"
-        f"type of error: {type_error}\n"
-        f"message of error: {message_error}\n"
+        f"\n"
+        f"error in function: {func_name}\n"
+        f"type of error: {error_type}\n"
+        f"message of error: {message_of_error}\n"
         f"error line: {error_line}\n"
         f"error line num: {error_line_num}"
     )
@@ -35,5 +39,7 @@ def return_message(func_name, type_error, message_error, error_line, error_line_
 
 
 def extract_error_line_from_stacktrace():
+    error_type, message_of_error = str(sys.exc_info()[0]), sys.exc_info()[1]
+    error_type = error_type[error_type.find("'") : -1]
     stacktrace = traceback.extract_tb(sys.exc_info()[2])[-1]
-    return stacktrace.line, stacktrace.lineno
+    return stacktrace.line, stacktrace.lineno, error_type, message_of_error
--- tests/Test/test2/task2/test_safe_call.py
+++ tests/Test/test2/task2/test_safe_call.py
@@ -9,23 +9,64 @@ def foo(a):
     return a
 
 
+@safe_call
+def goo(b):
+    if type(b) == str:
+        raise ValueError("b must be  not str.")
+    return foo(b)
+
+
 @pytest.mark.parametrize(
-    "func, input_arg, expected_line, expected_num",
+    "func, input_arg, expected_line, expected_num, expected_type_error, expected_error_message",
     [
-        (foo, 0, 'raise ValueError("a must be more or equal 100.")', 8),
-        (foo, "abc", "if a < 100:", 7),
+        (
+            foo,
+            0,
+            'raise ValueError("a must be more or equal 100.")',
+            8,
+            "'ValueError'",
+            "a must be more or equal 100.",
+        ),
+        (
+            foo,
+            "abc",
+            "if a < 100:",
+            7,
+            "'TypeError'",
+            "'<' not supported between instances of 'str' and 'int'",
+        ),
+        (
+            foo,
+            [],
+            "if a < 100:",
+            7,
+            "'TypeError'",
+            "'<' not supported between instances of 'list' and 'int'",
+        ),
     ],
 )
 def test_extract_error_line_from_stacktrace(
-    func, input_arg, expected_line, expected_num
+    func,
+    input_arg,
+    expected_line,
+    expected_num,
+    expected_type_error,
+    expected_error_message,
 ):
-    with warnings.catch_warnings(record=True) as w:
+    with warnings.catch_warnings(record=True):
         try:
             func(input_arg)
         except Exception:
-            output_line, output_num = extract_error_line_from_stacktrace()
+            (
+                output_line,
+                output_num,
+                output_type_error,
+                output_error_message,
+            ) = extract_error_line_from_stacktrace()
             assert output_line == expected_line
             assert output_num == expected_num
+            assert output_type_error == expected_type_error
+            assert output_error_message == expected_error_message
 
 
 @pytest.mark.parametrize(
@@ -34,38 +75,32 @@ def test_extract_error_line_from_stacktrace(
         (
             foo,
             12,
-            (
-                "\n"
-                "error in function: foo\n"
-                "type of error: 'ValueError'\n"
-                "message of error: a must be more or equal 100.\n"
-                'error line: raise ValueError("a must be more or equal 100.")\n'
-                "error line num: 8"
-            ),
+            "\n"
+            "error in function: foo\n"
+            "type of error: 'ValueError'\n"
+            "message of error: a must be more or equal 100.\n"
+            'error line: raise ValueError("a must be more or equal 100.")\n'
+            "error line num: 8",
         ),
         (
             foo,
             0,
-            (
-                "\n"
-                "error in function: foo\n"
-                "type of error: 'ValueError'\n"
-                "message of error: a must be more or equal 100.\n"
-                'error line: raise ValueError("a must be more or equal 100.")\n'
-                "error line num: 8"
-            ),
+            "\n"
+            "error in function: foo\n"
+            "type of error: 'ValueError'\n"
+            "message of error: a must be more or equal 100.\n"
+            'error line: raise ValueError("a must be more or equal 100.")\n'
+            "error line num: 8",
         ),
         (
             foo,
             "asv",
-            (
-                "\n"
-                "error in function: foo\n"
-                "type of error: 'TypeError'\n"
-                "message of error: '<' not supported between instances of 'str' and 'int'\n"
-                "error line: if a < 100:\n"
-                "error line num: 7"
-            ),
+            "\n"
+            "error in function: foo\n"
+            "type of error: 'TypeError'\n"
+            "message of error: '<' not supported between instances of 'str' and 'int'\n"
+            "error line: if a < 100:\n"
+            "error line num: 7",
         ),
     ),
 )
@@ -84,7 +119,7 @@ def test_safe_call_with_err(func, input_number, expected):
 def test_safe_call_without_err(input_number, expected):
     with warnings.catch_warnings(record=True) as w:
         output = foo(input_number)
-        assert output == int(input_number)
+        assert output == input_number
         assert len(w) == 0
 
 
@@ -97,14 +132,12 @@ def test_safe_call_without_err(input_number, expected):
             'can only concatenate str (not "list") to str',
             "print(a + b)",
             "40",
-            (
-                "\n"
-                "error in function: foo\n"
-                "type of error: 'TypeError'\n"
-                'message of error: can only concatenate str (not "list") to str\n'
-                "error line: print(a + b)\n"
-                "error line num: 40"
-            ),
+            "\n"
+            "error in function: foo\n"
+            "type of error: 'TypeError'\n"
+            'message of error: can only concatenate str (not "list") to str\n'
+            "error line: print(a + b)\n"
+            "error line num: 40",
         ),
         (
             "gogogo",
@@ -112,14 +145,25 @@ def test_safe_call_without_err(input_number, expected):
             "PIOMON",
             "print(a + b)",
             "666",
-            (
-                "\n"
-                "error in function: gogogo\n"
-                "type of error: 'ErrorError'\n"
-                "message of error: PIOMON\n"
-                "error line: print(a + b)\n"
-                "error line num: 666"
-            ),
+            "\n"
+            "error in function: gogogo\n"
+            "type of error: 'ErrorError'\n"
+            "message of error: PIOMON\n"
+            "error line: print(a + b)\n"
+            "error line num: 666",
+        ),
+        (
+            "big mistake in code...",
+            "would i fix it?",
+            "yes!",
+            "i'm developer!",
+            "I will fix this tests!",
+            "\n"
+            "error in function: big mistake in code...\n"
+            "type of error: would i fix it?\n"
+            "message of error: yes!\n"
+            "error line: i'm developer!\n"
+            "error line num: I will fix this tests!",
         ),
     ),
 )
@@ -130,3 +174,57 @@ def test_return_message(
         return_message(func_name, type_error, message_error, error_line, error_line_num)
         == expected
     )
+
+
+@pytest.mark.parametrize(
+    "func, input_num, expected",
+    [
+        (
+            goo,
+            12,
+            "\n"
+            "error in function: foo\n"
+            "type of error: 'ValueError'\n"
+            "message of error: a must be more or equal 100.\n"
+            'error line: raise ValueError("a must be more or equal 100.")\n'
+            "error line num: 8",
+        ),
+        (
+            goo,
+            "abc",
+            "\n"
+            "error in function: goo\n"
+            "type of error: 'ValueError'\n"
+            "message of error: b must be  not str.\n"
+            'error line: raise ValueError("b must be  not str.")\n'
+            "error line num: 15",
+        ),
+        (
+            goo,
+            0.13,
+            "\n"
+            "error in function: foo\n"
+            "type of error: 'ValueError'\n"
+            "message of error: a must be more or equal 100.\n"
+            'error line: raise ValueError("a must be more or equal 100.")\n'
+            "error line num: 8",
+        ),
+        (
+            goo,
+            [],
+            "\n"
+            "error in function: foo\n"
+            "type of error: 'TypeError'\n"
+            "message of error: '<' not supported between instances of 'list' and 'int'\n"
+            "error line: if a < 100:\n"
+            "error line num: 7",
+        ),
+    ],
+)
+def test_safe_call_nested_func(func, input_num, expected):
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        func(input_num)
+        assert len(w) == 1
+        assert issubclass(w[-1].category, Warning)
+        assert expected == str(w[-1].message)
