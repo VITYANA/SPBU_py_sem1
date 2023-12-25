33f4b1e4edb8a9181884503ada0d2ed599b11b34
Виктор Ханукаев <144259984+VITYANA@users.noreply.github.com>
2023-12-25 20:16:07 +0300

    simlified fsm, modified main, fix tests.

--- src/Practice/prac9/task1/FSM.py
+++ src/Practice/prac9/task1/FSM.py
@@ -3,34 +3,27 @@ from dataclasses import dataclass
 
 @dataclass
 class FSMachine:
-    states: list[dict[str:int]]
+    states: list[dict[str, int]]
     start_state: int
     accepted_states: list[int]
 
 
 def create_fs_machine(
-    given_states: list[dict[str:int]], start_state: int, accepted_states: list[int]
+    given_states: list[dict[str, int]], start_state: int, accepted_states: list[int]
 ):
-    all_states = []
-    for state in given_states:
-        all_states.append(state)
-    return FSMachine(all_states, start_state, accepted_states)
+    return FSMachine(given_states, start_state, accepted_states)
+
+
+def move_state(symbol, state, fsm):
+    for key, value in fsm.states[state].items():
+        if symbol in key:
+            return value
 
 
 def validate_string(fsm: FSMachine, string: str):
-    current_state = 0
+    current_state = fsm.start_state
     for symbol in string:
-        current_state = fsm.states[current_state]
-        transfer = list(
-            map(
-                lambda condition: current_state[condition]
-                if symbol in condition
-                else None,
-                current_state,
-            )
-        )
-        next_state = list(filter(lambda cond: cond is not None, transfer))
-        if len(next_state) == 0:
+        current_state = move_state(symbol, current_state, fsm)
+        if current_state is None:
             return False
-        current_state = next_state[0]
     return current_state in fsm.accepted_states
--- src/Practice/prac9/task1/main.py
+++ src/Practice/prac9/task1/main.py
@@ -2,37 +2,51 @@ from src.Practice.prac9.task1.FSM import *
 import string
 
 
-def digits_validation(input_str: str) -> bool:
-    table_digits = [
-        {string.digits: 1, "+-": 5},
-        {string.digits: 1, "E": 3, ".": 6},
-        {string.digits: 2, "E": 3},
-        {string.digits: 4, "+-": 7},
-        {string.digits: 4},
-        {string.digits: 1},
-        {string.digits: 2},
-        {string.digits: 4},
-    ]
-    return validate_string(create_fs_machine(table_digits, 0, [1, 2, 4]), input_str)
+LANGUAGES = [
+    [
+        "abb-type language",
+        [{"b": 0, "a": 1}, {"b": 2, "a": 1}, {"b": 3, "a": 1}, {"b": 0, "a": 1}],
+        0,
+        [3],
+    ],
+    [
+        "digits language",
+        [
+            {string.digits: 1, "+-": 5},
+            {string.digits: 1, "E": 3, ".": 6},
+            {string.digits: 2, "E": 3},
+            {string.digits: 4, "+-": 7},
+            {string.digits: 4},
+            {string.digits: 1},
+            {string.digits: 2},
+            {string.digits: 4},
+        ],
+        0,
+        [1, 2, 4],
+    ],
+]
 
 
-def abb_validation(input_str: str) -> bool:
-    table_abb = [{"b": 0, "a": 1}, {"b": 2, "a": 1}, {"b": 3, "a": 1}, {"b": 0, "a": 1}]
-    return validate_string(create_fs_machine(table_abb, 0, [3]), input_str)
+def make_fsms():
+    fsms = []
+    for i in range(len(LANGUAGES)):
+        fsms.append(create_fs_machine(*LANGUAGES[i][1:]))
+    return fsms
 
 
-def output_match(user_input: str) -> str:
-    if abb_validation(user_input):
-        return "This is abb-type language."
-    elif digits_validation(user_input):
-        return "This is Digits language."
-    else:
-        return "This string match no language."
+def output_match(fsms, user_input: str) -> str:
+    lang_found_flag = False
+    for i in range(len(fsms)):
+        if validate_string(fsms[i], user_input):
+            lang_found_flag = True
+            return f"String belongs to {LANGUAGES[i][0]}."
+    if lang_found_flag is False:
+        return f"String didn't belongs to any language."
 
 
 def main():
     user_input = input("Input string for validation: ")
-    print(output_match(user_input))
+    print(output_match(make_fsms(), user_input))
 
 
 if __name__ == "__main__":
